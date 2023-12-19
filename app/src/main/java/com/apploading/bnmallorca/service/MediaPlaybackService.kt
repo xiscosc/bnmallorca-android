4627235039b6a111579fd4240bb2bcc7c82235c0
Xisco Sastre Cabot <francesc.sastrecabot@showpad.com>
2023-12-19 17:06:22 +0100

    touch notification

--- app/src/main/java/com/apploading/bnmallorca/service/MediaPlaybackService.kt
+++ app/src/main/java/com/apploading/bnmallorca/service/MediaPlaybackService.kt
@@ -1,21 +1,38 @@
 package com.apploading.bnmallorca.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import androidx.annotation.OptIn
+import androidx.core.app.NotificationCompat
 import androidx.media3.common.ForwardingPlayer
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.CommandButton
+import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.MediaStyleNotificationHelper
+import com.apploading.bnmallorca.MainActivity
 import com.apploading.bnmallorca.R
 import com.apploading.bnmallorca.bncore.TrackManager
+import com.google.common.collect.ImmutableList
 
 class MediaPlaybackService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
-    private val defaultAlbumArt = "android.resource://com.apploading.bnmallorca/drawable/album_placeholder"
+    private val defaultAlbumArt =
+        "android.resource://com.apploading.bnmallorca/drawable/album_placeholder"
+    private lateinit var nBuilder: NotificationCompat.Builder
+    private lateinit var notificationManager: NotificationManager
 
     private val preferenceListener = SharedPreferences.OnSharedPreferenceChangeListener { _, _ ->
         val track = TrackManager.getTrackFromNotificationSharedPreferences(this)
@@ -34,14 +51,20 @@ class MediaPlaybackService : MediaSessionService() {
                 .build()
 
             mediaSession!!.player.replaceMediaItem(0, itemCopy)
+
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+                nBuilder.setSubText(track.artist)
+                nBuilder.setContentTitle(track.name)
+                notificationManager.notify(1, nBuilder.build())
+            }
         }
     }
 
-    @OptIn(UnstableApi::class) override fun onCreate() {
+    @OptIn(UnstableApi::class)
+    override fun onCreate() {
         super.onCreate()
 
 
-        val context = this
         TrackManager.storePlayingStatus(false, this)
         val sharedPreferences = TrackManager.getSharedPreferencesForNotification(this)
         sharedPreferences.registerOnSharedPreferenceChangeListener(preferenceListener)
@@ -49,13 +72,14 @@ class MediaPlaybackService : MediaSessionService() {
         val player = ExoPlayer.Builder(this).build()
         val forwardingPlayer = object : ForwardingPlayer(player) {
             override fun play() {
-                val track = TrackManager.getTrackFromSharedPreferences(context)
+                val track = TrackManager.getTrackFromSharedPreferences(this@MediaPlaybackService)
                 val current = MediaItem.fromUri(Uri.parse(getString(R.string.stream_url)))
                 val metaCopy = current.mediaMetadata
                     .buildUpon()
                     .setArtist(track?.artist ?: "Bn Mallorca Radio")
                     .setTitle(track?.name ?: "Independencia Musical")
-                    .setArtworkUri(Uri.parse(track?.let { TrackManager.getAlbumArtUrl(it) } ?: defaultAlbumArt))
+                    .setArtworkUri(Uri.parse(track?.let { TrackManager.getAlbumArtUrl(it) }
+                        ?: defaultAlbumArt))
                     .build()
 
                 val itemCopy = current.buildUpon()
@@ -65,21 +89,69 @@ class MediaPlaybackService : MediaSessionService() {
                 this.setMediaItem(itemCopy)
                 this.prepare()
                 super.play()
-                TrackManager.storePlayingStatus(true, context)
+                TrackManager.storePlayingStatus(true, this@MediaPlaybackService)
             }
 
             override fun pause() {
                 super.stop()
-                TrackManager.storePlayingStatus(false, context)
+                TrackManager.storePlayingStatus(false, this@MediaPlaybackService)
             }
 
             override fun stop() {
                 super.stop()
-                TrackManager.storePlayingStatus(false, context)
+                TrackManager.storePlayingStatus(false, this@MediaPlaybackService)
             }
         }
 
         mediaSession = MediaSession.Builder(this, forwardingPlayer).build()
+        this.setMediaNotificationProvider(object : MediaNotification.Provider {
+            override fun createNotification(
+                mediaSession: MediaSession,
+                customLayout: ImmutableList<CommandButton>,
+                actionFactory: MediaNotification.ActionFactory,
+                onNotificationChangedCallback: MediaNotification.Provider.Callback
+            ): MediaNotification {
+                createNotification(mediaSession)
+                return MediaNotification(1, nBuilder.build())
+            }
+
+            override fun handleCustomCommand(
+                session: MediaSession,
+                action: String,
+                extras: Bundle
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    @OptIn(UnstableApi::class)
+    fun createNotification(session: MediaSession) {
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                "bnmallorca",
+                "Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+        )
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        nBuilder = NotificationCompat.Builder(this, "bnmallorca")
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(androidx.media3.session.R.drawable.media3_notification_small_icon)
+            .setStyle(MediaStyleNotificationHelper.MediaStyle(session))
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            val track = TrackManager.getTrackFromSharedPreferences(this@MediaPlaybackService)
+            nBuilder.setSubText(track?.artist ?: "Bn Mallorca Radio")
+            nBuilder.setContentTitle(track?.name ?: "Independencia Musical")
+            nBuilder.setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.album_placeholder))
+        }
     }
 
     // Remember to release the player and media session in onDestroy
